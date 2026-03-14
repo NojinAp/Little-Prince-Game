010cbfecae532f7c9ef8897b99080afad6fda1fc
Nozhin Azarpanah <azarpann@mcmaster.ca>
2026-03-14 18:05:11 -0400

    Fixed tilting

--- js/script.js
+++ js/script.js
@@ -81,7 +81,7 @@ window.addEventListener("load", function () {
             if (Math.abs(normalized) < deadzone) {
                 tiltInput = 0;
             } else {
-                    tiltInput = isMobileTiltDevice() ? -normalized : normalized;
+                    tiltInput = normalized;
             }
         });
 
